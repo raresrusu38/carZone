b3394186935f9d60305495f216b1e06dad264dec
raresrusu38 <rares.rusu29@gmail.com>
2020-11-26 20:50:09 +0200

    fixed urls to top bar, added about, services, contact

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,7 @@ app_name = "pages"
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('services', views.services, name="services"),
+    path('contact', views.contact, name="contact"),
 ]
