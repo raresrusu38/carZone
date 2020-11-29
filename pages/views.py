7917795b4b34e8c34929e90dd6b7e75985e3e220
raresrusu38 <rares.rusu29@gmail.com>
2020-11-29 11:14:37 +0200

    featured cars, latest cars and image gallery

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from pages.models import Teams
-
+from cars.models import *
 
 
 def home(request):
     teams = Teams.objects.all().order_by('-id')
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    latest_cars = Car.objects.all().order_by('-created_date')
     data = {
         'teams' : teams,
+        'featured_cars' : featured_cars,
+        'latest_cars' : latest_cars
     }
     return render(request, 'pages/home.html', data)
 
